0ef0d22694d6e951ec5d535e8f813d3e264d7d29
jonyv <jonyv@FERNANDO>
2020-10-21 20:36:00 +0200

    Proyect Library

---
+++ Reto1Library/src/comicSans/com/Reto1Library/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comicSans.com.Reto1Library;
+
+/**
+ *
+ * @author jonyv
+ */
+public class User {
+    
+}
