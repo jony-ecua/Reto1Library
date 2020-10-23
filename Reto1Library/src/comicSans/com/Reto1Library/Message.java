37a823762e63c86a432c2de72f4de341fcf5c272
jonyv <jonyv@FERNANDO>
2020-10-23 12:39:05 +0200

    Desarrollo de la class message

---
+++ Reto1Library/src/comicSans/com/Reto1Library/Message.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comicSans.com.Reto1Library;
+
+/**
+ * Clase para definir los enum
+ * @author Jonathan Viñan
+ */
+
+public enum Message{
+       
+        SIGN_IN, //Para el proceso de inicio seccion
+        SIGN_UP, //Para el proceso de registrado en la bda
+        LOGOUT,  //Para el proceso de salida del usuario del programa
+        ERROR_USESR_EXIST, //Para el proceso comprobar el usuario ya existe
+        ERROR_USER_NOT_EXIST,//Para el proceso de comprobar que el usuario no existe
+        ERROR_PASSWORD     //Para el proceos de comprobar la contraseña no es correcta   
+        
+    }   
+
